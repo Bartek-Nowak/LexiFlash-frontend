97cbc688c35108d62527763883343e87417acdc0
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-16 14:24:48 +0200

    🛠️ refactor: clean up starter files from Vite Vue template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <FlashCard />
 </template>
+<script setup lang="ts">
+import FlashCard from "@/components/FlashCard.vue"
+</script>
---
+++ src/components/FlashCard.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus culpa illo, quos aliquam dolorem enim, temporibus, facere repellat nam placeat ab in sit impedit soluta minus cum. Molestiae, optio dolore?</p>
+</template>
+<script setup lang="ts">
+
+</script>
