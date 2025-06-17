66615bab88dea816e16dd7f1008db0a41ab712b3
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-17 15:17:16 +0200

    ✨ feat: update LearningView to display ProgressBar and ErrorCount components

--- src/views/LearningView.vue
+++ src/views/LearningView.vue
@@ -1,4 +1,13 @@
 <template>
-  <p>LearningView</p>
+  <div class="flex w-full items-center p-5">
+    <ProgressBar class="flex-grow" :progress="progress" />
+    <ErrorCount :errors="errors" />
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { ErrorCount, ProgressBar } from '@/components';
+
+const errors = ref(0);
+const progress = ref(0);
+</script>
