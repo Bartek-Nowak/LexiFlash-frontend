40fa99ba62671239d2e1e3f2d25d9842b37ed1a5
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-17 10:17:50 +0200

    ✨ feat: replace language selection dropdown with LanguageSwitcher component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <select @change="changeLanguage">
-    <option value="pl">Polski</option>
-    <option value="en">English</option>
-  </select>
+  <LanguageSwitcher />
   <DeckView />
 </template>
 
 <script setup lang="ts">
 import DeckView from '@/views/DeckView.vue';
-import { useI18n } from 'vue-i18n';
-import { loadLocaleMessages } from '@/utils/useLocaleLoader.ts';
-
-const { locale } = useI18n();
-
-const changeLanguage = async (event: Event) => {
-  const target = event.target as HTMLSelectElement;
-  const newLocale = target.value;
-  await loadLocaleMessages(newLocale);
-  locale.value = newLocale;
-};
+import { LanguageSwitcher } from '@/components';
 </script>
