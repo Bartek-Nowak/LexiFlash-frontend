592e0f11e94c311060ee8e9ca8e7a001a33b00e2
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-17 10:07:57 +0200

    ✨ feat: implement language selection and internationalization support in App component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
+  <select @change="changeLanguage">
+    <option value="pl">Polski</option>
+    <option value="en">English</option>
+  </select>
   <DeckView />
 </template>
+
 <script setup lang="ts">
 import DeckView from '@/views/DeckView.vue';
+import { useI18n } from 'vue-i18n';
+import { loadLocaleMessages } from '@/utils/useLocaleLoader.ts';
+
+const { locale } = useI18n();
+
+const changeLanguage = async (event: Event) => {
+  const target = event.target as HTMLSelectElement;
+  const newLocale = target.value;
+  await loadLocaleMessages(newLocale);
+  locale.value = newLocale;
+};
 </script>
