dc82091cec807da1c728e72e6dec357f3e465717
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-17 13:28:37 +0200

    ✨ feat: update HomeView to use i18n for greetings and introductions; enhance UI with dynamic button labels and add PieChart component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="text-center mt-10">
-    <h1 class="text-3xl font-bold mb-4">Witaj w LexiFlash 👋</h1>
-    <p class="mb-8 text-gray-600">Ucz się szybciej dzięki fiszkom</p>
+    <h1 class="text-3xl font-bold mb-4">
+      {{ $t('greeting') }} {{ appName }} 👋
+    </h1>
+    <p class="mb-8 text-gray-600">{{ $t('introduction') }}</p>
     <div class="flex justify-center gap-4">
-      <Button variant="primary" @btn-click="router.push('/deck-list')"
-        >Rozpocznij naukę</Button
-      >
-      <Button variant="secondary" @btn-click="router.push('/deck-manage')"
-        >Zarządzaj taliami</Button
-      >
+      <Button variant="primary" @btn-click="router.push('/deck-list')">{{
+        $t('startLearning')
+      }}</Button>
+      <Button variant="secondary" @btn-click="router.push('/deck-manage')">{{
+        $t('pages.deckManage')
+      }}</Button>
     </div>
+    <PieChart
+      title="Test"
+      :data="[
+        { value: 100, name: 'Test 1' },
+        { value: 20, name: 'Test 2' },
+      ]"
+    />
   </div>
 </template>
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
-import { Button } from '@/components';
+import { Button, PieChart } from '@/components';
+import { computed } from 'vue';
 
 const router = useRouter();
+
+const appName = computed(() => import.meta.env.VITE_APP_NAME);
 </script>
