db0652d23c8e0cc6761379da70510ce1af6f3c8f
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-18 16:09:46 +0200

    ✨ refactor: simplify DeckView and HomeView components for improved readability and functionality

--- src/views/DeckView.vue
+++ src/views/DeckView.vue
@@ -1,24 +1,33 @@
 <template>
-  <div class="flex items-center justify-between w-full pt-11">
-    <!-- <FlashcardViewer
-      v-if="!flashcardsStore.isLoading && flashcardsStore.flashcards.length > 0"
-      :flashcards="flashcardsStore.flashcards"
-      :is-loading="flashcardsStore.isLoading"
-      :flashcard-index="0"
-    /> -->
-  </div>
+  <DefaultLayout>
+    <template #sidebar>
+      <ItemList
+        shape="classic"
+        class="text-white"
+        :header="flashcardStore.currentCategory?.name"
+        :items="flashcardStore.decks"
+        @item-selected="selectDeck"
+      />
+    </template>
+
+    <FlashcardViewer v-if="flashcardStore.currentDeck" />
+  </DefaultLayout>
 </template>
+
 <script setup lang="ts">
 import { onMounted } from 'vue';
-// import { useFlashcardStore } from '@/stores';
-// import { FlashcardViewer } from '@/components';
-import { useRoute } from 'vue-router';
+import { DefaultLayout } from '@/layout';
+import { ItemList, FlashcardViewer } from '@/components';
+import { useFlashcardStore } from '@/stores';
 
-const route = useRoute();
-// const deckId = route.params.id as string;
-// const flashcardsStore = useFlashcardStore();
+const flashcardStore = useFlashcardStore();
 
-onMounted(() => {
-  // flashcardsStore.fetchFlashcards(deckId);
+const selectDeck = (deck: any) => {
+  flashcardStore.setCurrentDeck(deck);
+};
+
+onMounted(async () => {
+  await flashcardStore.fetchDeck();
+  console.log('Mounted DeckView', flashcardStore.currentCategory);
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,38 +4,42 @@
       :header="flashcardStore.currentCategory?.name"
       :description="flashcardStore.currentCategory?.description"
     />
+
     <ItemList
+      v-if="flashcardStore?.decks?.length"
       shape="bordered"
-      :items="
-        flashcardStore.currentCategory
-          ? flashcardStore.decks[flashcardStore.currentCategory.name]
-          : []
-      "
+      :items="flashcardStore.decks"
+      @item-selected="selectDeck"
     />
   </DefaultLayout>
 </template>
+
 <script setup lang="ts">
 import { onMounted, watch } from 'vue';
 import { DefaultLayout } from '@/layout';
 import { ItemList, PackCard } from '@/components';
 import { useFlashcardStore } from '@/stores';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const flashcardStore = useFlashcardStore();
 
 watch(
   () => flashcardStore.currentCategory,
   async (newCategory) => {
     if (newCategory) {
-      await flashcardStore.fetchDeck(newCategory);
+      await flashcardStore.fetchDeck();
     }
   },
   { immediate: true }
 );
 
+const selectDeck = (deck: any) => {
+  flashcardStore.setCurrentDeck(deck);
+  router.push(`/deck/${deck.id}`);
+};
+
 onMounted(async () => {
-  await flashcardStore.fetchCategories();
-  if (flashcardStore.currentCategory) {
-    await flashcardStore.fetchDeck(flashcardStore.currentCategory);
-  }
+  await flashcardStore.fetchDeck();
 });
 </script>
