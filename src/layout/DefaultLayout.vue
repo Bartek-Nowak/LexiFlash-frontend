5e37a36dd11d1cea3ee07e2cb0ead871c5d79594
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-18 11:04:46 +0200

    ✨ feat: add DefaultLayout component with sidebar and main content structure

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex min-h-screen">
+    <!-- Sidebar -->
+    <div class="bg-slate-600 p-1 w-1/4 divide-y">
+      <slot name="sidebar">
+        <Greeting />
+        <DecksList shape="classic" show-header class="text-white" />
+      </slot>
+    </div>
+    <!-- Main Content -->
+    <div class="flex flex-col flex-1">
+      <main class="flex-1">
+        <slot></slot>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Greeting, DecksList } from '@/components';
+</script>
