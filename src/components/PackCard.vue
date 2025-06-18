639afb93ca8d1648946fa366a72c830feb2e3644
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-18 12:41:36 +0200

    ✨ feat: update PackCard component to use props for header and description

--- src/components/PackCard.vue
+++ src/components/PackCard.vue
@@ -2,8 +2,10 @@
   <div class="p-4">
     <div class="flex items-center space-x-4">
       <div class="flex flex-col gap-2">
-        <h2 class="text-xl font-semibold uppercase">asdf asd</h2>
-        <p class="text-gray-600">Mastery: asd fasd f</p>
+        <h2 v-if="header" class="text-xl font-semibold uppercase">
+          {{ header }}
+        </h2>
+        <p v-if="description" class="text-gray-600">{{ description }}</p>
         <p class="text-gray-600">Cards Studied: asdf asdf / asdf afsdf asd f</p>
         <div class="flex gap-2">
           <Button
@@ -25,4 +27,9 @@
 </template>
 <script setup lang="ts">
 import { Button } from '@/components';
+
+defineProps<{
+  header?: string;
+  description?: string;
+}>();
 </script>
