660900911462b60bbdb682b16a1f9a47c46ab9a4
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-18 11:04:54 +0200

    ✨ feat: refactor HomeView to use DefaultLayout and include PackCard and DecksList components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,15 @@
 <template>
-  <div class="text-center mt-10">
-    <h1 class="text-3xl font-bold mb-4">
-      {{ $t('greeting') }} {{ appName }} 👋
-    </h1>
-    <p class="mb-8 text-gray-600">{{ $t('introduction') }}</p>
-    <div class="flex justify-center gap-4">
-      <Button variant="primary" @btn-click="router.push('/learning')">{{
-        $t('startLearning')
-      }}</Button>
-      <Button variant="secondary" @btn-click="router.push('/deck-manage')">{{
-        $t('pages.deckManage')
-      }}</Button>
-    </div>
-    <PieChart
-      title="Test"
-      :data="[
-        { value: 100, name: 'Test 1' },
-        { value: 20, name: 'Test 2' },
-      ]"
+  <DefaultLayout>
+    <PackCard
+      pack-name="1"
+      icon-url="1"
+      :mastery="1"
+      :cardsStudied="{ current: 1, total: 1 }"
     />
-  </div>
+    <DecksList />
+  </DefaultLayout>
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-import { Button, PieChart } from '@/components';
-import { computed } from 'vue';
-
-const router = useRouter();
-
-const appName = computed(() => import.meta.env.VITE_APP_NAME);
+import { DefaultLayout } from '@/layout';
+import { DecksList, PackCard } from '@/components';
 </script>
