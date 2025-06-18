2a98f1ecb49c115022a714a463b9e3ded671e740
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-18 18:31:44 +0200

    ✨ refactor: restructure LearningView component for improved layout and organization

--- src/views/LearningView.vue
+++ src/views/LearningView.vue
@@ -1,55 +1,72 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
-    <div class="flex w-full items-center pt-5 px-5 gap-4">
-      <ProgressBar class="flex-grow" :progress="progress" />
-      <ErrorCount :errors="errors" />
-    </div>
-    <div class="flex flex-col items-center justify-center gap-2">
-      <div class="flex gap-2">
-        <input
-          id="answer"
-          v-model="answer"
-          type="text"
-          class="border bg-white border-gray-300 rounded px-4"
-          :placeholder="$t('flashcard.answerPlaceholder')"
+  <DefaultLayout>
+    <template #sidebar>
+      <Timer :seconds="100" class="mt-auto w-full" />
+    </template>
+    <PackCard
+      :header="flashcardStore.currentCategory?.name"
+      :description="flashcardStore.currentCategory?.description"
+    />
+    <div class="flex flex-col items-center justify-center">
+      <div class="flex w-full items-center pt-5 px-5 gap-4">
+        <ProgressBar class="flex-grow" :progress="progress" />
+        <ErrorCount :errors="errors" />
+      </div>
+      <div class="flex flex-col items-center justify-center gap-2">
+        <div class="flex gap-2">
+          <input
+            id="answer"
+            v-model="answer"
+            type="text"
+            class="border bg-white border-gray-300 rounded px-4"
+            :placeholder="$t('flashcard.answerPlaceholder')"
+          />
+          <Button
+            :variant="checkButtonVariant"
+            ref="InputRef"
+            @btn-click="checkAnswer"
+            >Check ✔️</Button
+          >
+        </div>
+        <FlashcardViewer
+          v-if="
+            !flashcardStore.isLoading && flashcardStore.flashcards.length > 0
+          "
+          :flashcards="flashcardStore.flashcards"
+          :is-loading="flashcardStore.isLoading"
+          :flashcard-index="flashcardIndex"
         />
-        <Button
-          :variant="checkButtonVariant"
-          ref="InputRef"
-          @btn-click="checkAnswer"
-          >Check ✔️</Button
+        <div
+          v-else
+          class="bg-white min-h-[50vh] shadow-md rounded-lg w-md flex flex-col items-center justify-center animate-pulse text-center text-gray-500"
         >
-      </div>
-      <FlashcardViewer
-        v-if="
-          !flashcardsStore.isLoading && flashcardsStore.flashcards.length > 0
-        "
-        :flashcards="flashcardsStore.flashcards"
-        :is-loading="flashcardsStore.isLoading"
-        :flashcard-index="flashcardIndex"
-      />
-      <div
-        v-else
-        class="bg-white min-h-[50vh] shadow-md rounded-lg w-md flex flex-col items-center justify-center animate-pulse text-center text-gray-500"
-      >
-        {{ $t('flashcard.loading') }}
+          {{ $t('flashcard.loading') }}
+        </div>
       </div>
     </div>
-  </div>
+  </DefaultLayout>
 </template>
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue';
 import { useFlashcardStore } from '@/stores';
-import { Button, ErrorCount, FlashcardViewer, ProgressBar } from '@/components';
+import { DefaultLayout } from '@/layout';
+import {
+  Button,
+  ErrorCount,
+  FlashcardViewer,
+  PackCard,
+  ProgressBar,
+  Timer,
+} from '@/components';
 
 const errors = ref(0);
 const progress = ref(0);
-const flashcardsStore = useFlashcardStore();
+const flashcardStore = useFlashcardStore();
 const flashcardIndex = ref(0);
 const answer = ref('');
 
 const checkAnswer = () => {
-  const currentFlashcard = flashcardsStore.flashcards[flashcardIndex.value];
+  const currentFlashcard = flashcardStore.flashcards[flashcardIndex.value];
   if (
     currentFlashcard &&
     answer.value.trim().toLowerCase() ===
@@ -58,19 +75,19 @@ const checkAnswer = () => {
     flashcardIndex.value++;
     answer.value = '';
     progress.value =
-      (flashcardIndex.value / flashcardsStore.flashcards.length) * 100;
+      (flashcardIndex.value / flashcardStore.flashcards.length) * 100;
   } else {
     errors.value++;
   }
 };
 
 const checkButtonVariant = computed(() => {
-  return flashcardsStore.isLoading || answer.value.trim() === ''
+  return flashcardStore.isLoading || answer.value.trim() === ''
     ? 'disabled'
     : 'primary';
 });
 
 onMounted(() => {
-  flashcardsStore.fetchFlashcards('1');
+  // flashcardsStore.fetchFlashcards('1');
 });
 </script>
