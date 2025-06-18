633049dae8da15e95f8bf6c1b6b67a7d161c9f30
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-06-18 16:09:26 +0200

    ✨ refactor: restructure FlashcardViewer component for improved layout and state management

--- src/components/FlashcardViewer.vue
+++ src/components/FlashcardViewer.vue
@@ -1,15 +1,61 @@
 <template>
-  <div class="flex items-center justify-between w-full">
-    <Flashcard v-if="!isLoading" :flashcard="flashcards[flashcardIndex]" />
+  <div class="flex flex-col h-full w-full justify-between items-center">
+    <div class="flex-grow flex items-center justify-center w-full">
+      <Flashcard
+        v-if="flashcardStore.flashcards.length"
+        :flashcard="flashcardStore.flashcards[flashcardIndex]"
+      />
+    </div>
+    <div class="flex justify-center gap-2 mb-4">
+      <Button
+        v-if="flashcardStore.flashcards.length"
+        @click="previousFlashcard"
+        :disabled="flashcardIndex === 0"
+        theme="secondary"
+        shape="rounded"
+      >
+        Previous
+      </Button>
+
+      <Button
+        v-if="flashcardStore.flashcards.length"
+        @click="nextFlashcard"
+        theme="success"
+        shape="rounded"
+      >
+        Next
+      </Button>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import type { FlashcardType } from '@/types';
-import { Flashcard } from '@/components';
+import { onMounted, watch, ref } from 'vue';
+import { Button, Flashcard } from '@/components';
+import { useFlashcardStore } from '@/stores';
 
-defineProps<{
-  flashcards: FlashcardType[];
-  isLoading: boolean;
-  flashcardIndex: number;
-}>();
+const flashcardStore = useFlashcardStore();
+
+const flashcardIndex = ref(0);
+
+watch(
+  () => flashcardStore.currentDeck,
+  async () => await flashcardStore.fetchFlashcards()
+);
+
+const nextFlashcard = () => {
+  if (flashcardStore.flashcards.length > 0) {
+    flashcardIndex.value =
+      (flashcardIndex.value + 1) % flashcardStore.flashcards.length;
+  }
+};
+
+const previousFlashcard = () => {
+  if (flashcardStore.flashcards.length > 0) {
+    flashcardIndex.value =
+      (flashcardIndex.value - 1 + flashcardStore.flashcards.length) %
+      flashcardStore.flashcards.length;
+  }
+};
+
+onMounted(async () => await flashcardStore.fetchFlashcards());
 </script>
